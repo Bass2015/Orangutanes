0a02a533a952ba2e4bf6a0b2ad5e8540aebe09e6
Sebastián Dolgonos <bass.dolg@gmail.com>
2022-10-25 21:37:58 +0200

    cositas

---
+++ Sebastian/constants.py
@@ -0,0 +1,9 @@
+
+DT = 'decission_tree'
+SUBJECTS = ['Karl', 'Locky', 'Jawie', 'Storma', 'Popo', 'Hadia']
+PERIODS = ['Prejuego', 'Juego', 'Postjuego']
+SPLIT_RATIO = 0.15
+SCORE = 'score'
+CM = 'confusion_matrix'
+RF = 'random_forest'
+SVM = 'svm'
