9a5aa0b6c57d2680e64dd6fe579aa9329676cb20
Sebastián Dolgonos <bass.dolg@gmail.com>
2023-01-18 23:25:12 +0100

    added selectbox to boxplot, desc for subjects

--- src/screens.py
+++ src/screens.py
@@ -28,22 +28,16 @@ class SubjectDashboard:
             with col4: 
                 self.stacked_bars()
 
-    
     def info(self):
         st.markdown(f'# {self.subject}')
-        wr('''Lorem ipsum dolor si amet, consectetur 
-        adipiscing incidunt ut labore et dolore magna aliquam
-         erat nostrud exercitation ullamcorper suscipit laboris
-          nis duis autem vel eum irure dolor in reprehenderit i,
-           dolore eu fugiat nulla pariatur. At vero eos et accusa
-            praesant luptatum delenit aigue duos dolor et mole
-             provident, simil tempor sunt in culpa qui officia de
-              fuga. Et harumd dereud facilis est er expedit disti 
-              eligend optio congue nihil impedit doming id quod 
-              assumenda est, omnis dolor repellend.''')
+        wr(CONFIG['DESC'][self.subject])
 
     def boxplot(self):
-        chart = Boxplot(self.subject, title='Mean time in behavior', df=self.df)
+        behavior = st.selectbox('Behavior', CONFIG['DATASET']['macro_bhv'])
+        chart = Boxplot(self.subject,
+                        title=f'Time in {behavior}',
+                        behavior=behavior,
+                        df=self.df)
         st.plotly_chart(chart.figure(), use_container_width=True)
     
     def stacked_bars(self):
@@ -136,6 +130,7 @@ class PieChart(Chart):
                      names='macro_bhv', 
                      color='macro_bhv', 
                      color_discrete_map=CONFIG['COLORS']['behaviors'])
+        self.fig.update_traces(textinfo='none')
         self.update_layout(title=self.title)
         return self.fig
     
@@ -164,6 +159,9 @@ class StackedBars(Chart):
         self.df['relative_freq'] = 0
         for period in self.df['period'].unique():
             self.calculate_rel_frequencies(period)
+        periods = ['pregame', 'game', 'postgame']
+        self.df['period'] = pd.Categorical(self.df['period'], categories=periods)
+        self.df.sort_values(by='period', inplace=True)
 
     def calculate_rel_frequencies(self, period):
         sum = self.df.groupby('period')['freq'].sum()
@@ -172,15 +170,22 @@ class StackedBars(Chart):
         self.df['relative_freq'] += rel_freqs
 
 class Boxplot(Chart):
-    def __init__(self, subject, title, df=None):
+    def __init__(self, subject, title, behavior, df=None):
+        self.behavior = behavior
         Chart.__init__(self, subject, title, df)
 
     def figure(self):
         if not isinstance(self.subject, list):
             subjects=[self.subject]
-        df = self.df[(self.df['subject'].isin(subjects))&(self.df['macro_bhv']=='Individual')]
-        self.fig = px.box(df, x='period', y='duration', color='period', width=500, title='Individual')
-        self.update_layout(self.fig, title='Mean time with behavior', lgd_title=None)
+        mask = (self.df['subject'].isin(subjects))&(self.df['macro_bhv']==self.behavior)
+        df = self.df[mask]
+        self.fig = px.box(df,
+                          x='period',
+                          y='duration',
+                          color='macro_bhv', 
+                          title='Individual',
+                          color_discrete_map=CONFIG['COLORS']['behaviors'])
+        self.update_layout(self.title, lgd_title=None)
         self.fig.update_layout({'showlegend': False, 
-                           'yaxis': {'title': 'seconds'}})
+                                'yaxis': {'title': 'seconds'}})
         return self.fig
